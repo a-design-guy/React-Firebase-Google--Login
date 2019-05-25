e82bfaccdde9a545ba0f7b4b9857fa260a531238
Vashisth Bhushan <vashisthbhushan@gmail.com>
2019-05-25 09:59:07 +0530

    React navigation

--- React-google-login/App.js
+++ React-google-login/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {createSwitchNavigator} frm 'react-navigation'
 export default class App extends React.Component {
   render() {
     return (
